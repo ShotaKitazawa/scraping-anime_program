d16601dbaf7f8fa1c7187022ca8a382c32a9fdad
ShotaKitazawa <skitazawa1121@gmail.com>
2017-02-17 20:10:19 +0900

    first commit

---
+++ broadcaster.py
@@ -0,0 +1,12 @@
+# 対象: NHK Eテレ , 日本テレビ , テレビ朝日 , TBS , テレビ東京 , フジテレビ , TOKYO MX , AT-X
+
+broadcaster_list = [
+        "NHK Eテレ",
+        "日本テレビ",
+        "テレビ朝日",
+        "TBS",
+        "テレビ東京",
+        "フジテレビ",
+        "TOKYO MX",
+        "AT-X",
+        ]
