ee74ae4a1f113c5159603ce8aaf8f1475f18e349
ShotaKitazawa <skitazawa1121@gmail.com>
2017-02-22 10:17:09 +0900

    anime_db.sql modified

--- anime_db.sql
+++ anime_db.sql
@@ -13,58 +13,68 @@ drop table if exists userreview;
 drop table if exists userhighlight;
 
 create table anime(
-	anime_id int(6), title varchar(40), about varchar(500), 
-	brand_id int(6) , actor_id int(6), writer_id int(6), 
-	director_id int(6), op_id int(6), ed_id int(6), 
+	anime_id int, title varchar(40), about varchar(500), 
+	brand_id int , actor_id int, writer_id int, 
+	director_id int, op_id int, ed_id int, 
 	official_page varchar(2083), official_twitter varchar(2083), 
 	PRIMARY KEY(anime_id)
 );
 create table brand(
-	brand_id int(6), name varchar(20), 
+	brand_id int AUTO_INCREMENT, name varchar(20), 
 	PRIMARY KEY(brand_id)
 );
 create table actor(
-	actor_id int(6), name varchar(20), 
+	actor_id int AUTO_INCREMENT, name varchar(20), 
 	PRIMARY KEY(actor_id)
 ); 
 create table originwriter(
-	writer_id int(6), name varchar(20), 
+	writer_id int AUTO_INCREMENT, name varchar(20), 
 	PRIMARY KEY(writer_id)
 );
 create table director(
-	director_id int(6), name varchar(20), 
+	director_id int AUTO_INCREMENT, name varchar(20), 
 	PRIMARY KEY(director_id)
 );
 create table openingsong(
-	op_id int(6), name varchar(20), songer_id int(6), 
+	op_id int AUTO_INCREMENT, name varchar(20), songer_id int, 
 	PRIMARY KEY(op_id)
 );
 create table endingsong(
-	ed_id int(6), name varchar(20), songer_id int(6), 
+	ed_id int AUTO_INCREMENT, name varchar(20), songer_id int, 
 	PRIMARY KEY(ed_id)
 );
 create table songer(
-	songer_id int(6), name varchar(20), 
+	songer_id int AUTO_INCREMENT, name varchar(20), 
 	PRIMARY KEY(songer_id)
 );
 create table broadcaster(
-	broadcaster_id int(6), name varchar(20), 
+	broadcaster_id int, name varchar(20), 
 	PRIMARY KEY(broadcaster_id)
 );
 create table broadcast_time(
-	anime_id int(6), broadcaster_id int(6), time varchar(20), 
+	anime_id int, broadcaster_id int, time varchar(20), 
 	PRIMARY KEY(anime_id, broadcaster_id)
 );
 create table users(
-	uid int(10), user_name varchar(10), pass_hash varchar(20), 
+	uid int(10) AUTO_INCREMENT, user_name varchar(10), pass_hash varchar(20), 
 	PRIMARY KEY(uid)
 );
 create table userreview(
-	uid int(10), anime_id int(6), tokuten int(3), 
+	uid int(10), anime_id int, tokuten int, 
 	tourokubi int, hitokoto varchar(200), 
 	PRIMARY KEY(anime_id, uid)
 );
 create table userhighlight(
-	uid int(10), anime_id int(6), broadcaster_id int(6), 
+	uid int(10), anime_id int, broadcaster_id int, 
 	PRIMARY KEY(uid, anime_id, broadcaster_id)
 );
+
+insert into broadcaster values (02, "NHK Eテレ");
+insert into broadcaster values (04, "日本テレビ");
+insert into broadcaster values (05, "テレビ朝日");
+insert into broadcaster values (06, "TBS");
+insert into broadcaster values (07, "テレビ東京");
+insert into broadcaster values (08, "フジテレビ");
+insert into broadcaster values (09, "TOKYO MX");
+insert into broadcaster values (10, "AT-X");
+
